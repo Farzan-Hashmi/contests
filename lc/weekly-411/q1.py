5cde44361091fc95e9fa92b5670ba44200bce905
Farzan Hashmi <farzan.ahmad.hashmi@gmail.com>
2024-08-21 00:53:10 -0700

    .

---
+++ lc/weekly-411/q1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countKConstraintSubstrings(self, s: str, k: int) -> int:
+        
+        def eligible(arr):
+            return Counter(arr)["1"] <= k or Counter(arr)["0"] <= k
+        
+        return sum(1 if eligible(s[l:r]) else 0 for l in range(len(s)) for r in range(l+1, len(s)+1))
+    
